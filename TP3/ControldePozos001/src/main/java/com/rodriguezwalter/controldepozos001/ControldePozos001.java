078bc197f23d4f476da182c03062cd54f8f7e687
Wallyrod <91105001+Wallyrod@users.noreply.github.com>
2024-11-17 20:38:49 -0300

    Add files via upload

---
+++ TP3/ControldePozos001/src/main/java/com/rodriguezwalter/controldepozos001/ControldePozos001.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.rodriguezwalter.controldepozos001;
+
+/**
+ *
+ * @author zihk
+ */
+public class ControldePozos001 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
+public class SensorData {
+    private double pressure;
+    private double flow;
+    private double temperature;
+    private double liquidLevel;
+
+    // Getters y Setters
+}
